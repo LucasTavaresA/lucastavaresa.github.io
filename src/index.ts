ab907058c46dd7c6ac12fab8788fc6030c7983e3
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-03-13 09:45:27 -0300

    Removido clip_svg

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,6 @@ const octokit = new Octokit();
 const star_svg: Readonly<string> = `<svg aria-label="star" role="img" height="16" viewBox="0 0 16 12" version="1.1" width="16" data-view-component="true">
   <path fill="white" fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
 </svg>`;
-const clip_svg: Readonly<string> = `<svg aria-hidden="true" role="img" viewBox="0 0 16 16" width="30" height="16" fill="currentColor" style="display: inline; user-select: none; overflow: visible;">
-  <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z" />
-</svg>`;
 
 const repos: ReadonlyArray<string> = [
     "Google",
@@ -33,7 +30,7 @@ async function GetRepoInfo(repo: string) {
             li.appendChild(name);
             if (response.data.homepage != "") {
                 name.setAttribute("href", response.data.homepage!);
-                name.innerHTML = `<p>` + response.data.name + clip_svg + `</p>`;
+                name.innerHTML = `<p>` + response.data.name + ` 🌎</p>`;
             } else {
                 name.setAttribute("href", response.data.html_url);
                 name.textContent = response.data.name;

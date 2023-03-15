2716458b2d455662c91d6e2b740d84accb103395
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-03-15 15:21:45 -0300

    Código pesadamente simplificado

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,69 @@
-import { Octokit } from '@octokit/rest';
+import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit();
 
-const star_svg: Readonly<string> = `<svg aria-label="star" role="img" height="16" viewBox="0 0 16 12" version="1.1" width="16" data-view-component="true">
-  <path fill="white" fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" />
-</svg>`;
+const star_svg: Readonly<string> = `<svg aria-label="star" role="img" height="16" viewBox="0 0 16 12" version="1.1" width="16" data-view-component="true"><path fill="white" fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z" /></svg>`;
 
 const repos: ReadonlyArray<string> = [
-    "Google",
-    "Jokenpo",
-    "calculadora-csharp",
-    "sgrade",
-    "Exercicios-CSharp-OOP",
-    "SingleComment.nvim",
+	"Google",
+	"Jokenpo",
+	"calculadora-csharp",
+	"sgrade",
+	"Exercicios-CSharp-OOP",
+	"SingleComment.nvim",
 ];
 
 const repolist = document.getElementById("projects") as HTMLUListElement;
 
 async function GetRepoInfo(repo: string) {
-    return octokit.rest.repos
-        .get({
-            owner: "LucasTavaresA",
-            repo: repo,
-        })
-        .then((response) => {
-            const li = document.createElement("li");
+	return octokit.rest.repos
+		.get({
+			owner: "LucasTavaresA",
+			repo: repo,
+		})
+		.then((response) => {
+			let homepage: string = "";
+			let name: string = "";
+			let topics: string = "";
 
-            const name = document.createElement("a");
-            li.appendChild(name);
-            if (response.data.homepage != "") {
-                name.setAttribute("href", response.data.homepage!);
-                name.innerHTML = `<p>` + response.data.name + ` 🌎</p>`;
-            } else {
-                name.setAttribute("href", response.data.html_url);
-                name.textContent = response.data.name;
-            }
+			if (response.data.homepage != "") {
+				homepage = response.data.homepage!;
+				name = `<p>` + response.data.name + ` 🌎</p>`;
+			} else {
+				homepage = response.data.html_url;
+				name = response.data.name;
+			}
 
-            const description = document.createElement("p");
-            li.appendChild(description);
-            description.textContent = response.data.description;
+			response.data.topics!.forEach((topic) => {
+				topics +=
+					`<li><a href="https://github.com/topics/` +
+					topic +
+					`">` +
+					topic +
+					`</a></li>`;
+			});
 
-            const topics = document.createElement("ul");
-            topics.setAttribute("class", "topics");
-            li.appendChild(topics);
-            response.data.topics!.forEach((topic) => {
-                const t = document.createElement("li");
-                const a = document.createElement("a");
-                t.appendChild(a);
-                a.setAttribute("href", "https://github.com/topics/" + topic);
-                a.innerText = topic;
-                topics.appendChild(t);
-            });
-
-            const info = document.createElement("div");
-            li.appendChild(info);
-            info.setAttribute("class", "info");
-
-            const language = document.createElement("div");
-            info.appendChild(language);
-            language.setAttribute("id", response.data.language!);
-            language.setAttribute("class", "language");
-
-            const lang_text = document.createElement("p");
-            info.appendChild(lang_text);
-            lang_text.textContent = response.data.language;
-
-            const svg = document.createElement("a");
-            info.appendChild(svg);
-            svg.setAttribute("id", "stars");
-            svg.setAttribute("href", response.data.html_url + "/stargazers");
-            svg.innerHTML = star_svg;
-
-            const stars = document.createElement("a");
-            info.appendChild(stars);
-            stars.setAttribute("id", "stars");
-            stars.setAttribute("href", response.data.html_url + "/stargazers");
-
-            const count = document.createElement("p");
-            stars.appendChild(count);
-            count.textContent = response.data.stargazers_count.toString();
-
-            repolist.prepend(li);
-        })
-        .catch((error: ErrorEvent) => {
-            console.error("ERROR: could not get repo information! \n" + error);
-        });
+			// prettier-ignore
+			repolist.innerHTML +=
+				`<li>` +
+				`<a href="` + homepage + `">` + name + `</a>` +
+				`<p>` + response.data.description + `</p>` +
+				`<ul class="topics">` + topics + `</ul>` +
+				`<div class="info">` +
+				`<div id="` + response.data.language + `" class="language"></div>` +
+				`<p>` + response.data.language + `</p>` +
+				`<a id="stars" href="` + response.data.html_url + `/stargazers">` + star_svg + `</a>` +
+				`<a id="stars" href="` + response.data.html_url + `/stargazers">` +
+				`<p>` + response.data.stargazers_count.toString() + `</p>` +
+				`</a>` +
+				`</div>` +
+				`</li>`;
+		})
+		.catch((error: ErrorEvent) => {
+			console.error("ERROR: could not get repo information! \n" + error);
+		});
 }
 
 repos.forEach((repo) => {
-    GetRepoInfo(repo);
+	GetRepoInfo(repo);
 });

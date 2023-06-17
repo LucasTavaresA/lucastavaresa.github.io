76033983c4e646cc4f243d22390aef34b9c2c40b
LucasTavaresA <tavares.lassuncao@gmail.com>
2023-06-17 01:06:04 -0300

    chore: Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'src'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+	entry: "./src/index.ts",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "src"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
 };
